366d94418fd1875d29676c1db66f9943c397ba4f
kyle <kdeak1210@gmail.com>
2017-12-15 03:07:58 -0500

    Updated ProfileSchema with city, gender, image (to be URL)

--- models/Profile.js
+++ models/Profile.js
@@ -4,6 +4,9 @@ const ProfileSchema = mongoose.Schema({
   username: {type: String, default: ''},
   password: {type: String, default: ''},
   email: {type: String, default: ''},
+  city: {type: String, default: ''},
+  gender: {type: String, default: ''},
+  image: {type: String, default: ''},
   timestamp: {type: Date, default: Date.now()}
 });
 
@@ -12,6 +15,9 @@ ProfileSchema.methods.summary = function(){
   const summary = {
     username: this.username,
     email: this.email,
+    city: this.city,
+    gender: this.gender,
+    image: this.image,
     timestamp: this.timestamp,
     id: this._id.toString()
   };
